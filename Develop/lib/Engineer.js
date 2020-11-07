2d8aa25f4bcfd19b724502c3b493771634b7bcaa
ziieng <cjengelhardt@gmail.com>
2020-11-06 22:15:37 -0800

    Engineer object tests passed

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,17 @@
+const Employee = require("../lib/Employee");
+
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super(name, id, email);
+        this.github = github;
+        this.role = "Engineer"
+    }
+
+    getGithub() {
+        let g = this.github
+        return g
+    }
+}
+
+    module.exports = Engineer
