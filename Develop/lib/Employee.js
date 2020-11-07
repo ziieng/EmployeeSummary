bf67c8cb23b8d5becd58f43cec13a8db291e275f
ziieng <cjengelhardt@gmail.com>
2020-11-06 20:49:01 -0800

    Employee object test passed

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,31 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name
+        this.id = id
+        this.email = email
+        this.role = "Employee"
+    }
+
+    getName() {
+        let n = this.name
+        return n
+    }
+
+    getId() {
+        let i = this.id
+        return i
+    }
+
+    getEmail() {
+        let e = this.email
+        return e
+    }
+
+    getRole() {
+        let r = this.role
+        return r
+    }
+    }
+
+    module.exports = Employee
